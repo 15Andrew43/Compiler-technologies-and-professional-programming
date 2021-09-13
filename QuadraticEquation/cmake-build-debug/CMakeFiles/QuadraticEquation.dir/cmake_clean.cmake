02a376cab2ae1e62e97544331eb57bd4a3fe4525
AndrewB <avborovets@gmail.com>
2021-09-13 16:36:04 +0300

    added project directory

---
+++ QuadraticEquation/cmake-build-debug/CMakeFiles/QuadraticEquation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuadraticEquation.dir/main.cpp.o"
+  "QuadraticEquation"
+  "QuadraticEquation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuadraticEquation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
