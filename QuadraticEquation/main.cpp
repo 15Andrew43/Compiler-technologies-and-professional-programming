d4fb1e7700e518b254bd8a7cff28d89ab022affa
AndrewB <avborovets@gmail.com>
2021-09-14 18:04:35 +0300

    added tests

--- QuadraticEquation/main.cpp
+++ QuadraticEquation/main.cpp
@@ -54,30 +54,113 @@ int SolveSquare (double a, double b, double c,
     }
 }
 
+void Test0Roots() {
+    double x1 = 0, x2 = 0;
+    printf("Testing cases, when 0 roots:\n");
+    printf("1. case: a = 0, b = 0, c != 0.\n");
+    assert(0 == SolveSquare(0, 0, 1, &x1, &x2));
+    printf("    1. Ok\n");
+    assert(0 == SolveSquare(0, 0, -1, &x1, &x2));
+    printf("    2. Ok\n");
+
+    printf("2. case: d < 0.\n");
+    assert(0 == SolveSquare(1, 1, 1, &x1, &x2));
+    printf("    1. Ok\n");
+    assert(0 == SolveSquare(-1, -1, -1, &x1, &x2));
+    printf("    2. Ok\n");
+
+    printf("\n\n");
+}
+
+void Test1Roots() {
+    double x1 = 0, x2 = 0;
+    printf("Testing cases, when 1 root:\n");
+    printf("1. case: a = 0, b != 0, c != 0.\n");
+    assert(1 == SolveSquare(0, 1, 2, &x1, &x2));
+    assert(x1 == -2);
+    printf("    1. Ok\n");
+    assert(1 == SolveSquare(0, 1, -2, &x1, &x2));
+    assert(x1 == 2);
+    printf("    2. Ok\n");
+
+    printf("2. case: x1 = x2.\n");
+    assert(1 == SolveSquare(1, 2, 1, &x1, &x2));
+    assert(x1 == -1);
+    printf("    1. Ok\n");
+    assert(1 == SolveSquare(1, -4, 4, &x1, &x2));
+    assert(x1 == 2);
+    printf("    2. Ok\n");
+
+    printf("\n\n");
+}
+
+void Test2Roots() {
+    double x1 = 0, x2 = 0;
+    printf("Testing cases, when 2 roots:\n");
+    assert(2 == SolveSquare(1, 7, 12, &x1, &x2));
+    assert(x1 == -4);
+    assert(x2 == -3);
+    printf("    1. Ok\n");
+    assert(2 == SolveSquare(1, 3, 2, &x1, &x2));
+    assert(x1 == -2);
+    assert(x2 == -1);
+    printf("    2. Ok\n");
+
+    assert(2 == SolveSquare(1, 0, -1, &x1, &x2));
+    assert(x1 == -1);
+    assert(x2 == 1);
+    printf("    3. Ok\n");
+    assert(2 == SolveSquare(1, 1, -6, &x1, &x2));
+    assert(x1 == -3);
+    assert(x2 == 2);
+    printf("    4. Ok\n");
+
+    printf("\n\n");
+}
+
+void TestInfRoots() {
+    double x1 = 0, x2 = 0;
+    printf("Testing cases, when Inf roots:\n");
+    assert(SS_INF_ROOTS == SolveSquare(0, 0, 0, &x1, &x2));
+    printf("    1. Ok\n");
+}
+
 int main() {
     printf("# Square equation solver\n"
            "# (c) Andrew Borovets, 2021\n\n");
-    printf("# Enter a, b, c: ");
-    double a = 0, b = 0, c = 0;
-    scanf("%lg %lg %lg", &a, &b, &c);
-    double x1 = 0, x2 = 0;
-    int nRoots = SolveSquare(a, b, c, &x1, &x2);
-    switch (nRoots) {
-        case 0:
-            printf("No roots\n");
-            break;
-        case 1:
-            printf("x = %lg\n", x1);
-            break;
-        case 2:
-            printf("x1 = %lg, x2 = %lg\n", x1, x2);
-            break;
-        case SS_INF_ROOTS:
-            printf("Any number");
-            break;
-        default:
-            printf("main(): ERROR: nRoots = %d\n",
-                   nRoots);
-            return 1;
+    printf("enter 0 - to solve the square equation\n"
+           "      other - run test\n");
+    int answer;
+    scanf("%d", &answer);
+    if (answer == 0) {
+        printf("# Enter a, b, c: ");
+        double a = 0, b = 0, c = 0;
+        scanf("%lg %lg %lg", &a, &b, &c);
+        double x1 = 0, x2 = 0;
+        int nRoots = SolveSquare(a, b, c, &x1, &x2);
+        switch (nRoots) {
+            case 0:
+                printf("No roots\n");
+                break;
+            case 1:
+                printf("x = %lg\n", x1);
+                break;
+            case 2:
+                printf("x1 = %lg, x2 = %lg\n", x1, x2);
+                break;
+            case SS_INF_ROOTS:
+                printf("Any number");
+                break;
+            default:
+                printf("main(): ERROR: nRoots = %d\n",
+                       nRoots);
+                return 1;
+        }
+    } else /* run tests*/ {
+        Test0Roots();
+        Test1Roots();
+        Test2Roots();
+        TestInfRoots();
+        printf("All tests are passed successfully!\n");
     }
 }
