a3de30ba19da99c7bd094e064a5aab030ad7adc5
AndrewB <avborovets@gmail.com>
2021-09-15 20:09:13 +0300

    modified enum, structure of tests

--- QuadraticEquation/main.cpp
+++ QuadraticEquation/main.cpp
@@ -5,16 +5,16 @@
 double eps = 0.0001;
 
 enum nRoots {
-    infinity = -1,
-    zero = 0,
-    one = 1,
-    two = 2
+    zero,
+    one,
+    two,
+    infinity
 };
 
 
 bool equal(double x, double y);
-int SolveLinear(double b, double c, double* x1);
-int SolveSquare (double a, double b, double c,
+nRoots SolveLinear(double b, double c, double* x1);
+nRoots SolveSquare (double a, double b, double c,
                  double* x1, double* x2);
 void Test0Roots();
 void Test1Roots();
@@ -29,9 +29,9 @@ int main() {
            "# (c) Andrew Borovets, 2021\n\n");
     printf("enter 0 - to solve the square equation\n"
            "      other - run test\n");
-    int answer = 0;
-    scanf("%d", &answer);
-    if (answer == 0) {
+    char answer = '0';
+    scanf("%c", &answer);
+    if (answer == '0') {
         printf("# Enter a, b, c: ");
         double a = 0, b = 0, c = 0;
         scanf("%lg %lg %lg", &a, &b, &c);
@@ -72,7 +72,7 @@ bool equal(double x, double y) {
 
 
 
-int SolveLinear(double b, double c, double* x1) {
+nRoots SolveLinear(double b, double c, double* x1) {
     if (equal(b, 0)) {
         return (equal(c, 0)) ? infinity : zero;
     }
@@ -96,7 +96,7 @@ int SolveLinear(double b, double c, double* x1) {
 //! \note   In case of infinite number of roots,
 //!         return SS_INF_ROOTS.
 //------------------------------------------------
-int SolveSquare (double a, double b, double c,
+nRoots SolveSquare (double a, double b, double c,
                  double* x1, double* x2) {
 
     assert(isfinite(a));
@@ -128,75 +128,126 @@ int SolveSquare (double a, double b, double c,
 }
 
 
-//      TODO
+
 void Test0Roots() {
     double x1 = 0, x2 = 0;
+    const int kn_tests = 2;
+    const int n_numbers = 3;
+
     printf("Testing cases, when 0 roots:\n");
+
     printf("1. case: a = 0, b = 0, c != 0.\n");
-    assert(0 == SolveSquare(0, 0, 1, &x1, &x2));
-    printf("    1. Ok\n");
-    assert(0 == SolveSquare(0, 0, -1, &x1, &x2));
-    printf("    2. Ok\n");
+    int tests1[kn_tests][n_numbers] = {
+            {0, 0, 1},
+            {0, 0, -1},
+    };
+    for (int i = 0; i < kn_tests; ++i) {
+        double a = tests1[i][0];
+        double b = tests1[i][1];
+        double c = tests1[i][2];
+        assert(zero == SolveSquare(a, b, c, &x1, &x2));
+        printf("    %d. Ok\n", i + 1);
+    }
 
     printf("2. case: d < 0.\n");
-    assert(0 == SolveSquare(1, 1, 1, &x1, &x2));
-    printf("    1. Ok\n");
-    assert(0 == SolveSquare(-1, -1, -1, &x1, &x2));
-    printf("    2. Ok\n");
+    int tests2[kn_tests][n_numbers] = {
+            {1, 1, 1},
+            {-1, -1, -1},
+    };
+    for (int i = 0; i < kn_tests; ++i) {
+        double a = tests2[i][0];
+        double b = tests2[i][1];
+        double c = tests2[i][2];
+        assert(zero == SolveSquare(a, b, c, &x1, &x2));
+        printf("    %d. Ok\n", i + 1);
+    }
 
     printf("\n\n");
 }
 
 void Test1Roots() {
     double x1 = 0, x2 = 0;
+    const int kn_tests = 2;
+    const int n_numbers = 4;
+
     printf("Testing cases, when 1 root:\n");
+
     printf("1. case: a = 0, b != 0, c != 0.\n");
-    assert(1 == SolveSquare(0, 1, 2, &x1, &x2));
-    assert(equal(x1, -2));
-    printf("    1. Ok\n");
-    assert(1 == SolveSquare(0, 1, -2, &x1, &x2));
-    assert(equal(x1, 2));
-    printf("    2. Ok\n");
+    int tests1[kn_tests][n_numbers] = {
+            {0, 1, 2, -2},
+            {0, 1, -2, 2},
+    };
+    for (int i = 0; i < kn_tests; ++i) {
+        double a = tests1[i][0];
+        double b = tests1[i][1];
+        double c = tests1[i][2];
+        double rights_x1 = tests1[i][3];
+        assert(one == SolveSquare(a, b, c, &x1, &x2));
+        assert(equal(x1, rights_x1));
+        printf("    %d. Ok\n", i + 1);
+    }
 
     printf("2. case: x1 = x2.\n");
-    assert(1 == SolveSquare(1, 2, 1, &x1, &x2));
-    assert(x1 == -1);
-    printf("    1. Ok\n");
-    assert(1 == SolveSquare(1, -4, 4, &x1, &x2));
-    assert(equal(x1, 2));
-    printf("    2. Ok\n");
+    int tests2[kn_tests][n_numbers] = {
+            {1, 2, 1, -1},
+            {1, -4, 4, 2},
+    };
+    for (int i = 0; i < kn_tests; ++i) {
+        double a = tests2[i][0];
+        double b = tests2[i][1];
+        double c = tests2[i][2];
+        double rights_x1 = tests2[i][3];
+        assert(one == SolveSquare(a, b, c, &x1, &x2));
+        assert(equal(x1, rights_x1));
+        printf("    %d. Ok\n", i + 1);
+    }
 
     printf("\n\n");
 }
 
 void Test2Roots() {
     double x1 = 0, x2 = 0;
-    printf("Testing cases, when 2 roots:\n");
-    assert(2 == SolveSquare(1, 7, 12, &x1, &x2));
-    assert(equal(x1, -4));
-    assert(equal(x2 ,-3));
-    printf("    1. Ok\n");
-    assert(2 == SolveSquare(1, 3, 2, &x1, &x2));
-    assert(equal(x1, -2));
-    assert(x2 == -1);
-    printf("    2. Ok\n");
+    const int kn_tests = 4;
+    const int n_numbers = 5;
 
-    assert(2 == SolveSquare(1, 0, -1, &x1, &x2));
-    assert(equal(x1, -1));
-    assert(equal(x2, 1));
-    printf("    3. Ok\n");
-    assert(2 == SolveSquare(1, 1, -6, &x1, &x2));
-    assert(equal(x1, -3));
-    assert(equal(x2, 2));
-    printf("    4. Ok\n");
+    printf("Testing cases, when 2 roots:\n");
+    int tests[kn_tests][n_numbers] = {
+            {1, 7, 12, -4, -3},
+            {1, 3, 2, -2, -1},
+            {1, 0, -1, -1, 1},
+            {1, 1, -6, -3, 2}
+    };
+    for (int i = 0; i < kn_tests; ++i) {
+        double a = tests[i][0];
+        double b = tests[i][1];
+        double c = tests[i][2];
+        double right_x1 = tests[i][3];
+        double right_x2 = tests[i][4];
+        assert(two == SolveSquare(a, b, c, &x1, &x2));
+        assert(equal(x1, right_x1));
+        assert(equal(x2, right_x2));
+        printf("    %d. Ok\n", i + 1);
+    }
 
     printf("\n\n");
 }
 
 void TestInfRoots() {
     double x1 = 0, x2 = 0;
-    printf("Testing cases, when Inf roots:\n");
-    assert(infinity == SolveSquare(0, 0, 0, &x1, &x2));
-    printf("    1. Ok\n");
-}
+    const int kn_tests = 1;
+    const int n_numbers = 3;
 
+    printf("Testing cases, when Inf roots:\n");
+    int tests[kn_tests][n_numbers] = {
+            {0, 0, 0},
+    };
+    for (int i = 0; i < kn_tests; ++i) {
+        double a = tests[i][0];
+        double b = tests[i][1];
+        double c = tests[i][2];
+        assert(infinity == SolveSquare(a, b, c, &x1, &x2));
+        printf("    %d. Ok\n", i + 1);
+    }
+
+    printf("\n\n");
+}
