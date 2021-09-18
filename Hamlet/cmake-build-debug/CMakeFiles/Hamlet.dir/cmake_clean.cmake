e9831cc87c5476bddab910796f49f57b9a1146ae
Андрей Боровец <avborovets@Mac-mini-Andrej.local>
2021-09-18 21:15:02 +0300

    done 1st part of ex: read from file, sorted arr with c-library qsort

---
+++ Hamlet/cmake-build-debug/CMakeFiles/Hamlet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hamlet.dir/main.cpp.o"
+  "Hamlet"
+  "Hamlet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hamlet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
