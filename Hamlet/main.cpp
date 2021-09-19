287c46999b0cc7e22750d4500443c8f739371c8f
Андрей Боровец <avborovets@Mac-mini-Andrej.local>
2021-09-19 12:45:35 +0300

    reverse_comp is done

--- Hamlet/main.cpp
+++ Hamlet/main.cpp
@@ -7,18 +7,24 @@ const int MAX_LENGTH = 256;
 
 
 void PrintSeparation();
-char** Copy(char** text, const int cnt_elem);
+void Print(char** text, int cnt_lines);
+
 int comp (const void* a, const void* b);
 int reverse_comp(const void* a, const void* b);
+
 char** ParseFileToArray(const char* string, const int cnt_lines);
+
 int getCntLines(const char *name);
+char** Copy(char** text, const int cnt_elem);
 void Free(char** str,char** copied_str, const int size);
-void Print(char** text, int cnt_lines);
+char* Reverse(char* str, const int size);
 
 void MyQiuckSort(char** base, size_t num, size_t size, int (*compare) (const void *, const void *));
 
 
-char* Reverse(char* str, const int size);
+
+
+
 
 int main() {
     const char file_name[] = "/Users/avborovets/Desktop/PROGA/COMPILER-TECHNOLOGIES-AND-PROFFESIONAL-PROGRAMMING/Compiler-technologies-and-professional-programming/Hamlet/hamlet.txt";
@@ -27,60 +33,54 @@ int main() {
     char** text = ParseFileToArray(file_name, cnt_lines);
     char** sorted_text = Copy(text, cnt_lines);
 
-    qsort(text, cnt_lines, sizeof(char*), reverse_comp);
-//    MyQiuckSort(sorted_text, cnt_lines - 1, sizeof(char*), comp);
+    qsort(sorted_text, cnt_lines, sizeof(char*), comp);
+    Print(sorted_text, cnt_lines);
+    PrintSeparation();
 
-//    Print(sorted_text, cnt_lines);
-//    PrintSeparation();
+    MyQiuckSort(sorted_text, cnt_lines - 1, sizeof(char*), reverse_comp);
+    Print(sorted_text, cnt_lines);
+    PrintSeparation();
+
     Print(text, cnt_lines);
-
 
 
     Free(text, sorted_text, cnt_lines);
     return 0;
 }
 
+
+
+
 void PrintSeparation() {
-    printf("===========================================================================================");
+    printf("\n\n\n===========================================================================================\n\n\n");
 }
 
-
-char** Copy(char** text, const int cnt_elem) {
-    char** new_text = (char**) calloc(cnt_elem, sizeof(char*));
-    for (int i = 0; i < cnt_elem; ++i) {
-        new_text[i] = text[i];
+void Print(char** text, int cnt_lines) {
+    for (int i = 0; i < cnt_lines; ++i) {
+        printf("%s", text[i]);
     }
-    return new_text;
 }
+
+
 
 int comp (const void* a, const void* b) {
     return strcmp(*(char**)a,*(char**)b); // wwwwwwwwwwhhhhhhhhhhyyyyyyyyyyy not strcmp( (char*)a, (char*)b )
 }
 
-
-// todo
 int reverse_comp(const void* a, const void* b) {
-    char* first = *(char**)a;
+    char* first = *(char**)a; // wwwwwwhhhhhhyyyyyy
     char* second = *(char**)b;
     int size_first = strlen(first);
     int size_second = strlen(second);
     char* reverse_first = Reverse(first, size_first);
     char* reverse_second = Reverse(second, size_second);
-
-//    printf("%s", reverse_first);
-
+    int res_cmp = strcmp(reverse_first, reverse_second);
     free(reverse_first);
     free(reverse_second);
-    return strcmp(reverse_first, reverse_second);
+    return res_cmp;
 }
 
-char* Reverse(char* str, const int size) {
-    char* reverse_str = (char*) calloc(size, sizeof(char));
-    for (int i = 0; i < size; ++i) {
-        reverse_str[size - i - 1] = str[i];
-    }
-    return reverse_str;
-}
+
 
 char** ParseFileToArray(const char* file_name, const int cnt_lines) {
     char cur_line[MAX_LENGTH] = "";
@@ -106,6 +106,8 @@ char** ParseFileToArray(const char* file_name, const int cnt_lines) {
     return text;
 }
 
+
+
 int getCntLines(const char* name) {
     char tmp_line[MAX_LENGTH] = "";
 
@@ -125,6 +127,13 @@ int getCntLines(const char* name) {
     return cnt_lines;
 }
 
+char** Copy(char** text, const int cnt_elem) {
+    char** new_text = (char**) calloc(cnt_elem, sizeof(char*));
+    for (int i = 0; i < cnt_elem; ++i) {
+        new_text[i] = text[i];
+    }
+    return new_text;
+}
 
 void Free(char** str, char** copied_str, const int size) {
     for (int i = 0; i < size; ++i) {
@@ -134,12 +143,16 @@ void Free(char** str, char** copied_str, const int size) {
     free(copied_str);
 }
 
-
-void Print(char** text, int cnt_lines) {
-    for (int i = 0; i < cnt_lines; ++i) {
-        printf("%s", text[i]);
+char* Reverse(char* str, const int size) {
+    char* reverse_str = (char*) calloc(size, sizeof(char));
+    for (int i = 0; i < size; ++i) {
+        reverse_str[size - i - 1] = str[i];
     }
+    return reverse_str;
 }
+
+
+
 
 
 
