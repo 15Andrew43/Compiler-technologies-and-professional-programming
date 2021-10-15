e14c354bc55f1f8ffb74a3ed882cb65e78a70d3e
AndrewB <avborovets@gmail.com>
2021-10-15 15:51:51 +0300

    open all files only 1 time

--- Hamlet/main.cpp
+++ Hamlet/main.cpp
@@ -7,18 +7,18 @@ const int MAX_LENGTH = 256;
 char* text;
 
 
-void PrintSeparation(const char* file_out, const char* mode);
-void Print(const char* text, const int cnt_lines, const int* indexes, const char *file_out, const char* mode);
-void PrintBeginning(const char* string, const char* file_out, const char* mode);
-void PrintEnd(const char* string, const char* file_out, const char* mode);
+void PrintSeparation(FILE *file);
+void Print(const char *text, const int cnt_lines, const int *indexes, FILE *file);
+void PrintBeginning(const char *string, FILE *file_out, FILE *file_in);
+void PrintEnd(const char *string, FILE *file_out, FILE *file_in);
 
 int comp (const void* a, const void* b);
 int reverse_comp(const void* a, const void* b);
 
-char* ParseFileToArray(const char* file_name, const int cnt_lines, const int file_size, int* indexes);
+char *ParseFileToArray(FILE *file, const int cnt_lines, const int file_size, int *indexes);
 
-int getCntLines(const char* file_name);
-int getFileSize(const char* file_name);
+int getCntLines(FILE *file);
+int getFileSize(FILE *file);
 int* Copy(int* indexes, const int cnt_elem);
 char* Reverse(char* str, const int size);
 int FindLetter(const char* str);
@@ -29,34 +29,58 @@ void MyQiuckSort(int* base, size_t num, size_t size, int (*cmp) (const void *, c
 
 
 int main() {
-    const char file_out[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/out.txt";
+    const char file_out_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/out.txt";
+    const char file_in_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/hamlet.txt";
+    const char file_begin_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/beginning.txt";
+    const char file_end_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/end.txt";
 
-    const char file_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/hamlet.txt";
-//    const char file_name[] = "/Users/avborovets/Desktop/PROGA/COMPILER-TECHNOLOGIES-AND-PROFFESIONAL-PROGRAMMING/Compiler-technologies-and-professional-programming/Hamlet/hamlet.txt";
-    int cnt_lines = getCntLines(file_name);
-    int file_size = getFileSize(file_name);
+    FILE* file_out = fopen(file_out_name, "w+");
+    if (file_out == NULL) {
+        printf("can not open file '%s'", file_out_name);
+        return -1;
+    }
+    FILE* file_in = fopen(file_in_name, "r");
+    if (file_in == NULL) {
+        printf("can not open file '%s'", file_in_name);
+        return -1;
+    }
+
+    FILE* file_begin = fopen(file_begin_name, "r");
+    if (file_begin == NULL) {
+        printf("can not open file '%s'", file_begin_name);
+        return -1;
+    }
+    FILE* file_end = fopen(file_end_name, "r");
+    if (file_end == NULL) {
+        printf("can not open file '%s'", file_end_name);
+        return -1;
+    }
+
+
+    int cnt_lines = getCntLines(file_in);
+    int file_size = getFileSize(file_in);
 
 
     int* indexes = (int*) calloc(cnt_lines, sizeof(int));
-    text = ParseFileToArray(file_name, cnt_lines, file_size, indexes);
+    text = ParseFileToArray(file_in, cnt_lines, file_size, indexes);
 
 
     int* sorted_indexes = Copy(indexes, cnt_lines);
     qsort(sorted_indexes, cnt_lines, sizeof(int), comp);
-    PrintBeginning("Sorted 'Hamlet' by the beginning of the lines:\n", file_out, "w");
-    Print(text, cnt_lines, sorted_indexes, file_out, "a");
-    PrintEnd("\n", file_out, "a");
-    PrintSeparation(file_out, "a");
+    PrintBeginning("Sorted 'Hamlet' by the beginning of the lines:\n", file_out, file_begin);
+    Print(text, cnt_lines, sorted_indexes, file_out);
+    PrintEnd("\n", file_out, file_end);
+    PrintSeparation(file_out);
 
     MyQiuckSort(sorted_indexes, cnt_lines, sizeof(int), reverse_comp);
-    PrintBeginning("Sorted 'Hamlet' by the end of the lines:\n", file_out, "a");
-    Print(text, cnt_lines, sorted_indexes, file_out, "a");
-    PrintEnd("\n", file_out, "a");
-    PrintSeparation(file_out, "a");
+    PrintBeginning("Sorted 'Hamlet' by the end of the lines:\n", file_out, file_begin);
+    Print(text, cnt_lines, sorted_indexes, file_out);
+    PrintEnd("\n", file_out, file_end);
+    PrintSeparation(file_out);
 
-    PrintBeginning("Original Hamlet\n", file_out, "a");
-    Print(text, cnt_lines, indexes, file_out, "a");
-    PrintEnd("\n", file_out, "a");
+    PrintBeginning("Original Hamlet\n", file_out, file_begin);
+    Print(text, cnt_lines, indexes, file_out);
+    PrintEnd("\n", file_out, file_end);
 
 
     free(indexes);
@@ -68,46 +92,46 @@ int main() {
 
 
 
-void PrintEnd(const char* string, const char* file_out, const char* mode) {
-    FILE* file = fopen(file_out, mode);
-    fwrite(string, sizeof(char), strlen(string), file);
-//    printf("%s\n", string);
-    fclose(file);
+void PrintEnd(const char *string, FILE *file_out, FILE *file_in) {
+    fseek(file_out, 0, SEEK_END);
 
-    const char file_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/end.txt";
+    fwrite(string, sizeof(char), strlen(string), file_out);
+//    printf("%s\n", string);
+
+
 //    const char file_name[] = "/Users/avborovets/Desktop/PROGA/COMPILER-TECHNOLOGIES-AND-PROFFESIONAL-PROGRAMMING/Compiler-technologies-and-professional-programming/Hamlet/end.txt";
-    int cnt_lines = getCntLines(file_name);
-    int file_size = getFileSize(file_name);
+    int cnt_lines = getCntLines(file_in);
+    int file_size = getFileSize(file_in);
     int* indexes = (int*) calloc(cnt_lines, sizeof(int));
-    char* beginning = ParseFileToArray(file_name, cnt_lines, file_size, indexes);
-    Print(beginning, cnt_lines, indexes, file_out, "a");
+    char* beginning = ParseFileToArray(file_in, cnt_lines, file_size, indexes);
+    Print(beginning, cnt_lines, indexes, file_out);
 
     free(indexes);
 }
 
-void PrintBeginning(const char* string, const char* file_out, const char* mode) {
-    FILE* file = fopen(file_out, mode);
-    fwrite(string, sizeof(char), strlen(string), file);
+void PrintBeginning(const char *string, FILE *file_out, FILE *file_in) {
+    fseek(file_out, 0, SEEK_END);
+
+    fwrite(string, sizeof(char), strlen(string), file_out);
 //    printf("%s\n", string);
-    fclose(file);
 
-    const char file_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/beginning.txt";
+//    const char file_name[] = "/Users/andrew_borovets/Desktop/proga/DED/Compiler-technologies-and-professional-programming/Hamlet/beginning.txt";
 //    const char file_name[] = "/Users/avborovets/Desktop/PROGA/COMPILER-TECHNOLOGIES-AND-PROFFESIONAL-PROGRAMMING/Compiler-technologies-and-professional-programming/Hamlet/beginning.txt";
-    int cnt_lines = getCntLines(file_name);
-    int file_size = getFileSize(file_name);
+    int cnt_lines = getCntLines(file_in);
+    int file_size = getFileSize(file_in);
     int* indexes = (int*) calloc(cnt_lines, sizeof(int));
-    char* beginning = ParseFileToArray(file_name, cnt_lines, file_size, indexes);
-    Print(beginning, cnt_lines, indexes, file_out, "a");
+    char* beginning = ParseFileToArray(file_in, cnt_lines, file_size, indexes);
+    Print(beginning, cnt_lines, indexes, file_out);
 
     free(indexes);
 }
 
-void PrintSeparation(const char* file_out, const char* mode) {
+void PrintSeparation(FILE *file) {
+    fseek(file, 0, SEEK_END);
+
     const char sep[] = "\n\n\n===========================================================================================\n\n\n";
-    FILE* file = fopen(file_out, mode);
     fwrite(sep, sizeof(char), strlen(sep), file);
 //    printf("%s", sep);
-    fclose(file);
 }
 
 //==========================================================================
@@ -118,16 +142,13 @@ void PrintSeparation(const char* file_out, const char* mode) {
 //! \param [in]     indexes     array, where displacements
 //!                                 from beginning of text are saved
 //==========================================================================
-void Print(const char* text, const int cnt_lines, const int* indexes, const char* file_out, const char* mode) {
-    FILE* file = fopen(file_out, mode);
+void Print(const char *text, const int cnt_lines, const int *indexes, FILE *file) {
+    fseek(file, 0, SEEK_END);
 
     for (int i = 0; i < cnt_lines; ++i) {
         fwrite(text + indexes[i], sizeof(char), strlen(text + indexes[i]), file);
 //        printf("%s", text + indexes[i]);
-
     }
-
-    fclose(file);
 }
 
 
@@ -172,7 +193,7 @@ int FindLetter(const char* str) {
 //! Opens file, reads it line by line
 //!             and save every line to dynamic memory
 //!
-//! \param [in]     file_name       name of file in format "/Users/.../file_name"
+//! \param [in]     file            FILE* which show on necessary file
 //! \param [in]     cnt_lines       count of lines in file
 //! \param [in]     file_size       size of file
 //! \param [in]     indexes         array, where displacements
@@ -181,16 +202,16 @@ int FindLetter(const char* str) {
 //!
 //! P.S. after this function indexes will be filled
 //==========================================================================
-char* ParseFileToArray(const char* file_name, const int cnt_lines, const int file_size, int* indexes) {
+char *ParseFileToArray(FILE *file, const int cnt_lines, const int file_size, int *indexes) {
+    fseek(file, 0, SEEK_SET);
 
     char* text = (char*) calloc(file_size + cnt_lines + 1, sizeof(char));
 
-    FILE* file = fopen(file_name,"r");
 
-    if(file == NULL) {
-        printf("can not open file '%s'", file_name);
-        return NULL;
-    }
+//    if(file == NULL) {
+//        printf("can not open file '%s'", file_name);
+//        return NULL;
+//    }
 
     char* cur_ptr = text;
     int ind_line = 0;
@@ -201,7 +222,7 @@ char* ParseFileToArray(const char* file_name, const int cnt_lines, const int fil
             ++ind_line;
         }
     }
-    fclose(file);
+
     return text;
 }
 
@@ -209,37 +230,42 @@ char* ParseFileToArray(const char* file_name, const int cnt_lines, const int fil
 //==========================================================================
 //! Counts number of lines in file
 //!
-//! \param [in]     name    name of file in format "/Users/.../file_name"
+//! \param [in]     file    FILE* which show on necessary file
 //! \return number of lines in file
 //==========================================================================
-int getCntLines(const char* name) {
+int getCntLines(FILE *file) {
+    fseek(file , 0, SEEK_SET);
+
     char tmp_line[MAX_LENGTH] = "";
 
-    FILE* file = fopen(name, "r");
-
-    if(file == NULL) {
-        printf("can not open file '%s' in getCntLines", name);
-        return NULL;
-    }
+//    if(file == NULL) {
+//        printf("can not open file '%s' in getCntLines", file);
+//        return NULL;
+//    }
     int cnt_lines = 0;
     while(!feof (file)) {
         if (fgets(tmp_line, MAX_LENGTH, file)) {
             ++cnt_lines;
         }
     }
-    fclose(file);
+
     return cnt_lines;
 }
 
 //==========================================================================
 //! Finds size of file
 //!
-//! \param file_name    name of file
+//! \param      file    FILE* which show on necessary file
 //! \return size of file in bytes
 //==========================================================================
-int getFileSize(const char* file_name) {
+int getFileSize(FILE *file) {
     struct stat st;
-    stat(file_name, &st);
+//    stat(file, &st);
+
+    if ((fstat(fileno(file), &st) != 0) || (!S_ISREG(st.st_mode))) {
+        return -1;
+    }
+
     return st.st_size;
 }
 
